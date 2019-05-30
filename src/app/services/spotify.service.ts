b2fc6cb3867768357082ff05b6e16a2d0071a0ac
IvonneBarco <ivonn1022@gmail.com>
2019-05-30 16:13:37 -0500

    Optimizando código

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,44 +1,40 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { map } from "rxjs/operators";
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class SpotifyService {
   list: any[] = [];
   token: any =
-    "BQDBzfo6lIPc75YI7TDH8-LmCwQcf6G6M2H24HDW4Y6f1swqO3h7DsMFtKuUFq9TpEAG0uQxmPy5HpPeO3A";
+    'BQDBzfo6lIPc75YI7TDH8-LmCwQcf6G6M2H24HDW4Y6f1swqO3h7DsMFtKuUFq9TpEAG0uQxmPy5HpPeO3A';
 
   constructor(private http: HttpClient) {
-    console.log("Spotify services ready");
+    console.log('Spotify services ready');
+  }
+
+  getQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${ query }`;
+
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer ' + this.token + ''
+    });
+
+    return this.http.get(url, { headers });
   }
 
   getNewReleases() {
-    const headers = new HttpHeaders({
-      Authorization: "Bearer " + this.token + ""
-    });
-
-    return this.http
-      .get("https://api.spotify.com/v1/browse/new-releases", {
-        headers
+    return this.getQuery('browse/new-releases')
+    .pipe(map(data => {
+        return data['albums'].items;
       })
-      .pipe(
-        map(data => {
-          return data["albums"].items;
-        })
-      );
+    );
+
   }
   getArtist(termino: string) {
-    const headers = new HttpHeaders({
-      Authorization: "Bearer " + this.token + ""
-    });
-
-    return this.http
-      .get(`https://api.spotify.com/v1/search?q=${{ termino }}&type=artist`, {
-        headers
-      })
-      .pipe(map(data => data["artists"].items));
+    return this.getQuery(`search?q=${{ termino }}&type=artist`)
+    .pipe(map(data => data['artists'].items));
   }
 }
